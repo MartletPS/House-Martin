2219ae2adedb247c0c470d3bdb3ff2ed59cd7bb3
Jeremy Vernon <jeremyvernon.com@gmail.com>
2014-05-02 14:27:52 -0700

    Initial Cuts
    - Adjusted background colour and text colours
    - Added the logo image with base size parameters
    - Updated screenshot with target render

--- header.php
+++ header.php
@@ -48,7 +48,7 @@
 				<div id="inner-header" class="wrap cf">
 
 					<?php // to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> ?>
-					<p id="logo" class="h1"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></p>
+					<a href="<?php echo home_url(); ?>" rel="nofollow"><img id="logo" src="<?= home_url(); ?>/wp-content/themes/house-martin/library/images/logo.png" /></a>
 
 					<?php // if you'd like to use the site description you can un-comment it below ?>
 					<?php // bloginfo('description'); ?>

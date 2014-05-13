ae4377c7b42841b34603f13687c23fe66f54ef04
Jeremy Vernon <jeremyvernon.com@gmail.com>
2014-05-13 10:30:21 -0700

    First cut at slider styling
    Creates a dependency on a particular slider plugin for styling.

--- index.php
+++ index.php
@@ -2,11 +2,14 @@
 
 			<div id="content">
 
-				<div id="inner-content" class="wrap cf">
+                <div id="inner-content" class="wrap cf">
 
-						<div id="main" class="m-all t-2of3 d-5of7 cf" role="main">
 
-							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                    <div id="main" class="m-all t-2of3 d-5of7 cf" role="main">
+
+                        <?php if(function_exists('show_flexslider_rotator')) echo show_flexslider_rotator( 'index' ); ?>
+
+                        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 							<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article">
 

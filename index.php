f260a5df7264e9ee66953bd4f19e4d652e3da65f
Jeremy Vernon <jeremyvernon.com@gmail.com>
2014-05-28 09:28:58 -0700

    Tiling of index posts
    posts on index page now appear in two columns of "tiles" with footer
    information removed.

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
 
                 <div id="inner-content" class="wrap cf">
 
-
                     <div id="main" class="m-all t-2of3 d-5of7 cf" role="main">
 
                         <?php if(function_exists('show_flexslider_rotator')) echo show_flexslider_rotator( 'index' ); ?>
+
 
                         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
@@ -16,9 +16,10 @@
 								<header class="article-header">
 
 									<h1 class="h2 entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
-									<p class="byline vcard">
+						<!--			<p class="byline vcard">
 										<?php printf( __( 'Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span>', 'bonestheme' ), get_the_time('Y-m-j'), get_the_time(get_option('date_format')), get_the_author_link( get_the_author_meta( 'ID' ) )); ?>
 									</p>
+						-->
 
 								</header>
 
@@ -26,11 +27,11 @@
 									<?php the_content(); ?>
 								</section>
 
+                        <!--
 								<footer class="article-footer cf">
 									<p class="footer-comment-count">
 										<?php comments_number( __( '<span>No</span> Comments', 'bonestheme' ), __( '<span>One</span> Comment', 'bonestheme' ), _n( '<span>%</span> Comments', '<span>%</span> Comments', get_comments_number(), 'bonestheme' ) );?>
 									</p>
-
 
                  	<?php printf( __( '<p class="footer-category">Filed under: %1$s</p>', 'bonestheme' ), get_the_category_list(', ') ); ?>
 
@@ -38,6 +39,7 @@
 
 
 								</footer>
+					    -->
 
 							</article>
 

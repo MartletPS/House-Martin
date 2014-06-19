7550d86610b2da2775f851a2470fc436dbc418e6
Jeremy Vernon <jeremyvernon.com@gmail.com>
2014-06-19 13:09:11 -0700

    Removed post article footer

--- post-formats/format.php
+++ post-formats/format.php
@@ -51,7 +51,7 @@
                     ) );
                   ?>
                 </section> <?php // end article section ?>
-
+<!---
                 <footer class="article-footer">
 
                   <?php printf( __( 'Filed under: %1$s', 'bonestheme' ), get_the_category_list(', ') ); ?>
@@ -59,7 +59,7 @@
                   <?php the_tags( '<p class="tags"><span class="tags-title">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '</p>' ); ?>
 
                 </footer> <?php // end article footer ?>
-
+-->
                 <?php comments_template(); ?>
 
               </article> <?php // end article ?>

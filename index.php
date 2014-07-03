ed7f049efb770c21f883b5316ad87b7ddcb2d450
TheMartlet <proco@martlet.ca>
2014-07-03 15:13:23 -0700

    undo

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
 									<h1 class="h2 entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
 					            <!--    <p class="byline vcard">By: <?php printf(get_the_author_link()); ?> </p> -->
 										<p class="byline vcard">
-										<?php printf( __( "by "'<span class="author">%3$s</span> <time class="updated" datetime="%1$s" pubdate>%2$s</time>', 'bonestheme' ), get_the_time('Y-m-j'), get_the_time(get_option('date_format')), the_author_posts_link( get_the_author_meta( 'ID' ) )); ?>
+										<?php printf( __( '<span class="author">%3$s</span> <time class="updated" datetime="%1$s" pubdate>%2$s</time>', 'bonestheme' ), get_the_time('Y-m-j'), get_the_time(get_option('date_format')), the_author_link( get_the_author_meta( 'ID' ) )); ?>
 									</p>
 						
 

330e4900f4849619c082c06e1bebcf46fa32fe67
TheMartlet <proco@martlet.ca>
2014-10-25 21:52:59 -0700

    author

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
 										<p class="byline vcard">
 										<?php printf( __( '<span class="author">%3$s</span> <time class="updated" datetime="%1$s" pubdate>%2$s</time>', 'bonestheme' ), get_the_time('Y-m-j'), get_the_time(get_option('date_format')), get_the_author_link( get_the_author_meta( 'ID' ) )); ?>
 									</p>
-						
+
 
 								</header>
 

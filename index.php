a13d1df17bef79e4a1b166d9964bbbd86bd48237
TheMartlet <proco@martlet.ca>
2014-10-29 22:04:51 -0700

    coauthors

--- index.php
+++ index.php
@@ -18,10 +18,10 @@
 									<h1 class="h2 entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
 
 										<p class="byline vcard">
-										
+
                                                                                     <?php
                                                                                         if ( function_exists( 'coauthors_posts_links' ) ) {
-                                                                                            printf( __( '<span class="author">%3$s</span> <time class="updated" datetime="%1$s">%2$s</time>', 'bonestheme' ), get_the_time('Y-m-j'), get_the_time(get_option('date_format')),  coauthors_posts_links(', ', 'and ', 'By ', $after = null) );
+                                                                                            printf( __( '<span class="author">%3$s</span> <time class="updated" datetime="%1$s">%2$s</time>', 'bonestheme' ), get_the_time('Y-m-j'), get_the_time(get_option('date_format')),  coauthors_posts_links(', ', 'and ', 'By ', '') );
                                                                                         } else {
                                                                                             printf( __( '<span class="author">%3$s</span> <time class="updated" datetime="%1$s">%2$s</time>', 'bonestheme' ), get_the_time('Y-m-j'), get_the_time(get_option('date_format')), the_author_posts_link() );
                                                                                         }

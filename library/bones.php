e7290f593dcbae73b9b5bad1d79626b1b0fd1f51
TheMartlet <proco@martlet.ca>
2014-11-06 22:25:35 -0800

    admin bar

--- library/bones.php
+++ library/bones.php
@@ -53,8 +53,6 @@ function bones_head_cleanup() {
 	// remove Wp version from scripts
 	add_filter( 'script_loader_src', 'bones_remove_wp_ver_css_js', 9999 );
 
-	add_filter('show_admin_bar', '__return_true');
-
         add_filter( 'show_admin_bar' , 'custom_admin_bar');
 
 } /* end bones head cleanup */

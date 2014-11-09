b2acc48e45b16104bc424497c8562498c1713e29
TheMartlet <proco@martlet.ca>
2014-11-09 14:42:57 -0800

    author corrections, img alt added

--- post-formats/format-link.php
+++ post-formats/format-link.php
@@ -7,7 +7,13 @@
                   <h1 class="entry-title single-title" itemprop="headline"><?php the_title(); ?></h1>
 
                   <p class="byline vcard">
-                    <?php printf( __( 'Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span>', 'bonestheme' ), get_the_time('Y-m-j'), get_the_time(get_option('date_format')), get_the_author_link( get_the_author_meta( 'ID' ) )); ?>
+                    <?php
+                        if ( function_exists( 'coauthors_posts_links' ) ) {
+                            printf( __( '<span class="author">%3$s</span> <time class="updated" datetime="%1$s">%2$s</time>', 'bonestheme' ), get_the_time('Y-m-j'), get_the_time(get_option('date_format')),  coauthors_posts_links(null,null, 'By ', null, false) );
+                        } else {
+                            printf( __( '<span class="author">%3$s</span> <time class="updated" datetime="%1$s">%2$s</time>', 'bonestheme' ), get_the_time('Y-m-j'), get_the_time(get_option('date_format')), the_author_posts_link() );
+                        }
+                    ?>
                   </p>
 
                 </header> <?php // end article header ?>

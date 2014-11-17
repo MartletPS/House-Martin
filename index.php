821be737b61cdeeeb045fe87c82adc342623b017
TheMartlet <proco@martlet.ca>
2014-11-16 16:09:43 -0800

    infinite scroll

--- index.php
+++ index.php
@@ -8,50 +8,11 @@
 
                         <?php if(function_exists('show_flexslider_rotator')) echo show_flexslider_rotator( 'index' ); ?>
 
-                        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
-							<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article">
-
-								<header class="article-header">
-
-									<h1 class="h2 entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
-
-										<p class="byline vcard">
-
-                                                                                    <?php
-                                                                                        if ( function_exists( 'coauthors_posts_links' ) ) {
-                                                                                            printf( __( '<span class="author">%3$s</span> <time class="updated" datetime="%1$s">%2$s</time>', 'bonestheme' ), get_the_time('Y-m-j'), get_the_time(get_option('date_format')),  coauthors_posts_links(null,null, 'By ', null, false) );
-                                                                                        } else {
-                                                                                            printf( __( '<span class="author">%3$s</span> <time class="updated" datetime="%1$s">%2$s</time>', 'bonestheme' ), get_the_time('Y-m-j'), get_the_time(get_option('date_format')), the_author_posts_link() );
-                                                                                        }
-                                                                                    ?>
-                                                                                </p>
-
-
-								</header>
-
-								<section class="entry-content cf">
-                                                                        <div class="fadeout">
-                                                                        <?php the_excerpt(); ?>
-                                                                        </div>
-								</section>
-
-                        <!--
-								<footer class="article-footer cf">
-									<p class="footer-comment-count">
-										<?php comments_number( __( '<span>No</span> Comments', 'bonestheme' ), __( '<span>One</span> Comment', 'bonestheme' ), _n( '<span>%</span> Comments', '<span>%</span> Comments', get_comments_number(), 'bonestheme' ) );?>
-									</p>
-
-                 	<?php printf( __( '<p class="footer-category">Filed under: %1$s</p>', 'bonestheme' ), get_the_category_list(', ') ); ?>
-
-                  <?php the_tags( '<p class="footer-tags tags"><span class="tags-title">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '</p>' ); ?>
-
-
-								</footer>
-					    -->
-                                                        </article>
-
-							<?php endwhile; ?>
+                        <?php
+                        while (have_posts()) : the_post();
+                            get_template_part( 'content', get_post_format() );
+                        endwhile;
+                        ?>
 
 									<?php bones_page_navi(); ?>
 

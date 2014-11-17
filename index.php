def857c08fa41142ca08136892709968e7198a67
TheMartlet <proco@martlet.ca>
2014-11-16 16:24:36 -0800

    infinite scroll

--- index.php
+++ index.php
@@ -11,7 +11,26 @@
                         <div id="postsContent">
                         <?php
                         while (have_posts()) : the_post();
-                            get_template_part( 'content', get_post_format() );
+
+    <article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article">
+        <header class="article-header">
+            <h1 class="h2 entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
+            <p class="byline vcard">
+                <?php
+                    if ( function_exists( 'coauthors_posts_links' ) ) {
+                          printf( __( '<span class="author">%3$s</span> <time class="updated" datetime="%1$s">%2$s</time>', 'bonestheme' ), get_the_time('Y-m-j'), get_the_time(get_option('date_format')),  coauthors_posts_links(null,null, 'By ', null, false) );
+                    } else {
+                          printf( __( '<span class="author">%3$s</span> <time class="updated" datetime="%1$s">%2$s</time>', 'bonestheme' ), get_the_time('Y-m-j'), get_the_time(get_option('date_format')), the_author_posts_link() );
+                    } // else
+                ?>
+              </p>
+        </header>
+        <section class="entry-content cf">
+            <div class="fadeout">
+                <?php the_excerpt(); ?>
+            </div>
+        </section>
+    </article>
                         endwhile;
                         ?>
                         </div>
